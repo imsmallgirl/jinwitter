27bd2ecf0aea3fb3581de068428d8bb0fa964e0e
yujin <d__wls_@naver.com>
2022-11-24 22:44:29 +0900

    end commit

--- src/components/JinRouter.jsx
+++ src/components/JinRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "../routes/Home";
 import Main from "../routes/Main";
 import Profile from "../routes/Profile";
